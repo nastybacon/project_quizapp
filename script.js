b679b4b45ddc51a2c42f923764a1ff1a9779dca3
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-03 21:04:26 +0100

    progress bar

--- script.js
+++ script.js
@@ -32,31 +32,33 @@ let questions = [
         "right_answer": 3
     }
 ];
-
 let rightQuestions = 0;
-
 let currentQuestion = 0;
-
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length; /** "Länge" der Fragen */
-
     showQuestion();
-
 };
-
 function showQuestion() {
-
     if (currentQuestion >= questions.length) { /** wenn der Wert der aktuellen Frage größer als die Menge der Fragen ist, dann */
-        // TODO: Show End Screen
+        // Show End Screen
         document.getElementById('endScreen').style = ''; // sichtbar machen
         document.getElementById('questionBody').style = 'display: none'; // ausblenden
+
         document.getElementById('amount-of-guestions').innerHTML = questions.length; // gesamte Menge der Fragen
         document.getElementById('amount-of-right-questions').innerHTML = rightQuestions; // gesamte Menge der richtigen Antworten
         document.getElementById('header-image').src = 'img/cup.jpg'; // Bild beim Ergebnis ersetzten
     } else { // wenn nicht, dann Frage anzeigen
 
-        let question = questions[currentQuestion]; /** wir machen einen Container und holen das erste Element aus dem Array raus */
+        let percent = (currentQuestion + 1) / questions.length; // ausrechnen wie viel Prozent eine Frage aktuell hat + (0+1) für die erste Frage
+        percent = Math.round(percent * 100); // % anzeigen + runden (nicht 0.25, sondern 25)
 
+        document.getElementById('progress-bar').innerHTML = `${percent}%`; // % mit Zahl*100 als Variable anzeigen 
+        document.getElementById('progress-bar').style = `width: ${percent}%`; // die Prozentzahl auch für den blauen Balken verändern        
+        
+        console.log('Fortschritt', percent);
+
+        let question = questions[currentQuestion]; /** wir machen einen Container und holen das erste Element aus dem Array raus */
+       
         document.getElementById('question-number').innerHTML = currentQuestion + 1; /** die Nummer (0+1) der aktuellen Frage anzeigen  */
         document.getElementById('questiontext').innerHTML = question['question'];
         document.getElementById('answer_1').innerHTML = question['answer_1'];
@@ -65,16 +67,13 @@ function showQuestion() {
         document.getElementById('answer_4').innerHTML = question['answer_4'];
     }
 }
-
 function answer(selection) {
     let question = questions[currentQuestion];
     console.log('Selected answer is ', selection); /** Selected answer is answer_2 */
     let selectedQuestionNumber = selection.slice(-1); /** .slice(-1) ist der letzte Buchstabe von selection (=answer_3) */
     console.log('SelectedQuestionNumber is', selectedQuestionNumber);
     console.log('Current question is ', question['right_answer']); /** Current question is 3 */
-
     let idOfRightAnswer = `answer_${question['right_answer']}`; /** ID der richtigen Antwort */
-
     if (selectedQuestionNumber == question['right_answer']) { /** wenn selected question = richtige Antwort, dann */
         document.getElementById(selection).parentNode.classList.add('bg-success'); /** .parentNode für "Überklasse" | Bootstrap Klasse für grüne Buttons hinzugefügt */
         rightQuestions++; // bei richtiger Antwort 0+1 | bei -- wird es um 1 verringert
@@ -85,14 +84,12 @@ function answer(selection) {
     }
     document.getElementById('next-button').disabled = false; /** .disabled = false -> Button bei Eingabe "freischalten" */
 }
-
 function nextQuestion() { /** Funktion für den "Nächste Frage" Button */
     currentQuestion++; /** wir erhöhen unsere Variable von 0 auf 1 */
     document.getElementById('next-button').disabled = true; /** .disabled = true -> Button bei nächster Frage wieder deaktivieren */
     resetAnswerButtons() /** Funktion zum resetten der Buttons aufrufen */
     showQuestion(); /** die funktion für die nächste Variable anzeigen */
 }
-
 function resetAnswerButtons() {
     document.getElementById('answer_1').parentNode.classList.remove('bg-danger'); /** die Klassen danger und success entfernen bei nächster Frage */
     document.getElementById('answer_1').parentNode.classList.remove('bg-success'); /** 8 mal für 4 Anworten */
@@ -102,6 +99,4 @@ function resetAnswerButtons() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
-
-
 }
